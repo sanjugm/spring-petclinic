26f223909708350edf077b6540a409efd8d9c4e7
sanju <sanjugm2344@gmail.com>
2026-08-18 21:26:09 +0530

    Format Terraform VM configuration

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -25,10 +25,10 @@ resource "azurerm_linux_virtual_machine" "sonarqube" {
     azurerm_network_interface.vm.id
   ]
 
- admin_ssh_key {
-  username   = "azureuser"
-  public_key = var.ssh_public_key
-}
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
 
   os_disk {
     caching              = "ReadWrite"
